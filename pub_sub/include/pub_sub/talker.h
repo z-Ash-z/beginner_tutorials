21a3edc7c001249f2614713dd849f16e44a7e5ee
Aneesh Chodisetty <ch.aneesh1996@gmail.com>
2022-11-18 03:00:41 -0500

    Made changes for Week10_HW submission

--- pub_sub/include/pub_sub/talker.h
+++ pub_sub/include/pub_sub/talker.h
@@ -4,16 +4,16 @@
  * @brief The talker node that publishes string messages to the specified topic.
  * @version 0.1
  * @date 2022-11-16
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
-#ifndef __TALKER_H__
-#define __TALKER_H__
+#ifndef PUB_SUB_INCLUDE_PUB_SUB_TALKER_H_
+#define PUB_SUB_INCLUDE_PUB_SUB_TALKER_H_
 
 #include <string>
-
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <pub_sub/srv/string_change.hpp>
@@ -26,14 +26,19 @@ class Talker : public rclcpp::Node {
    * @param node_name The name of the node which publishes the message.
    * @param service_name The name of the service that is provided.
    */
-  Talker(const std::string &node_name = "pub", std::string service_name = "change_publisher_string");
+  Talker(const std::string &node_name = "pub",
+         std::string service_name = "change_publisher_string");
 
  private:
-  rclcpp::TimerBase::SharedPtr timer_;  //!< The pointer that points to the callback.
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;  //!< The pointer to the publisher.
-  std_msgs::msg::String message_; //!< The message that will be broadcasted in the channel.
-  rclcpp::Service<pub_sub::srv::StringChange>::SharedPtr service_; //!< The pointer to the service.
-  
+  rclcpp::TimerBase::SharedPtr
+      timer_;  //!< The pointer that points to the callback.
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr
+      publisher_;  //!< The pointer to the publisher.
+  std_msgs::msg::String
+      message_;  //!< The message that will be broadcasted in the channel.
+  rclcpp::Service<pub_sub::srv::StringChange>::SharedPtr
+      service_;  //!< The pointer to the service.
+
   /**
    * @brief The callback funtion that prints and publishes a message in the
    * topic.
@@ -43,11 +48,13 @@ class Talker : public rclcpp::Node {
 
   /**
    * @brief The service callback that changes the string that is published.
-   * 
+   *
    * @param request The request message from client.
    * @param response The response sent to client after processing the request.
    */
-  void change_string(const std::shared_ptr<pub_sub::srv::StringChange::Request> request, std::shared_ptr<pub_sub::srv::StringChange::Response> response);
+  void change_string(
+      const std::shared_ptr<pub_sub::srv::StringChange::Request> request,
+      std::shared_ptr<pub_sub::srv::StringChange::Response> response);
 };  // Talker
 
-#endif  // __TALKER_H__
+#endif  // PUB_SUB_INCLUDE_PUB_SUB_TALKER_H_
--- pub_sub/src/talker.cpp
+++ pub_sub/src/talker.cpp
@@ -4,12 +4,12 @@
  * @brief The implementation of the talker class.
  * @version 0.1
  * @date 2022-11-16
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
-#include "talker.h"
+#include <talker.h>
 
 Talker::Talker(const std::string &node_name, std::string service_name)
     : Node(node_name) {
@@ -24,28 +24,47 @@ Talker::Talker(const std::string &node_name, std::string service_name)
   message_.data = this->get_parameter("publish_message").as_string();
 
   // Adding information in log file.
-  if (rcutils_logging_set_logger_level(this->get_name(), RCUTILS_LOG_SEVERITY::RCUTILS_LOG_SEVERITY_DEBUG) == RCUTILS_RET_OK) RCLCPP_DEBUG(this->get_logger(), "Started with DEBUG");
-  else RCLCPP_INFO(this->get_logger(), "Started without DEBUG");
-  RCLCPP_DEBUG(this->get_logger(), "[%s] started publishing '%s' to '/%s' at %imsec", node_name.c_str(), this->get_parameter("publish_message").as_string().c_str(), this->get_parameter("topic_name").as_string().c_str(), (int)this->get_parameter("publish_interval").as_int());
+  if (rcutils_logging_set_logger_level(
+          this->get_name(), RCUTILS_LOG_SEVERITY::RCUTILS_LOG_SEVERITY_DEBUG) ==
+      RCUTILS_RET_OK)
+    RCLCPP_DEBUG(this->get_logger(), "Started with DEBUG");
+  else
+    RCLCPP_INFO(this->get_logger(), "Started without DEBUG");
+  RCLCPP_DEBUG(this->get_logger(),
+               "[%s] started publishing '%s' to '/%s' at %imsec",
+               node_name.c_str(),
+               this->get_parameter("publish_message").as_string().c_str(),
+               this->get_parameter("topic_name").as_string().c_str(),
+               static_cast<int>(this->get_parameter("publish_interval").
+               as_int()));
 
   // Creating a publisher.
-  publisher_ = this->create_publisher<std_msgs::msg::String>(this->get_parameter("topic_name").as_string(), 10);
-  
+  publisher_ = this->create_publisher<std_msgs::msg::String>(
+      this->get_parameter("topic_name").as_string(), 10);
+
   // Creating a service.
-  service_ = this->create_service<pub_sub::srv::StringChange>(service_name, std::bind(&Talker::change_string, this, std::placeholders::_1, std::placeholders::_2));
+  service_ = this->create_service<pub_sub::srv::StringChange>(
+      service_name, std::bind(&Talker::change_string, this,
+                              std::placeholders::_1, std::placeholders::_2));
 
   // Starting the publisher.
-  timer_ = this->create_wall_timer(std::chrono::milliseconds(this->get_parameter("publish_interval").as_int()), std::bind(&Talker::timer_callback, this));
+  timer_ = this->create_wall_timer(
+      std::chrono::milliseconds(
+          this->get_parameter("publish_interval").as_int()),
+      std::bind(&Talker::timer_callback, this));
 
-  // Using FATAL level logging, not necessary for this usecase but just for this assignment.
-  if (this->get_parameter("publish_interval").as_int() < 500)
-  {
-    RCLCPP_FATAL(this->get_logger(), "The speed of publish is extremely high! Quiting.");
+  // Using FATAL level logging, not necessary for this usecase but just for this
+  // assignment.
+  if (this->get_parameter("publish_interval").as_int() < 500) {
+    RCLCPP_FATAL(this->get_logger(),
+                 "The speed of publish is extremely high! Quiting.");
     exit(2);
   }
 
-  // Using ERROR level logging, not necessary for this usecase but just for this assignment.
-  if (this->get_parameter("publish_interval").as_int() < 1000) RCLCPP_ERROR(this->get_logger(), "The speed of publish is too high!");
+  // Using ERROR level logging, not necessary for this usecase but just for this
+  // assignment.
+  if (this->get_parameter("publish_interval").as_int() < 1000)
+    RCLCPP_ERROR(this->get_logger(), "The speed of publish is too high!");
 }
 
 void Talker::timer_callback() {
@@ -56,10 +75,13 @@ void Talker::timer_callback() {
   publisher_->publish(message_);
 }
 
-void Talker::change_string(const std::shared_ptr<pub_sub::srv::StringChange::Request> request, std::shared_ptr<pub_sub::srv::StringChange::Response> response)
-{
+void Talker::change_string(
+    const std::shared_ptr<pub_sub::srv::StringChange::Request> request,
+    std::shared_ptr<pub_sub::srv::StringChange::Response> response) {
   message_.data = request->new_string;
-  RCLCPP_WARN(this->get_logger(), "String changed to : %s", request->new_string.c_str());
-  RCLCPP_DEBUG(this->get_logger(), "Now publishing '%s'", message_.data.c_str());
+  RCLCPP_WARN(this->get_logger(), "String changed to : %s",
+              request->new_string.c_str());
+  RCLCPP_DEBUG(this->get_logger(), "Now publishing '%s'",
+               message_.data.c_str());
   response->change_status = "OK";
 }
